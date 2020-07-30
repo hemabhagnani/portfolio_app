b5ff4ac7837b1a3283b360fc82f3a8da0ee79f1d
Hema bhagnani <hemabhagnani5@gmail.com>
2020-07-30 17:49:35 +0530

    Created frontend using widgets

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:Home()
+  ));
+}
+
+class Home extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: Text("WELCOME"),
+        centerTitle: true,
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("assets/pic1.jpg"),
+                      radius: 100.0,
+
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Divider(
+              height: 70.0,
+              color: Colors.black,
+            ),
+            Text(
+              "Name",
+              style:TextStyle(
+                color: Colors.black,
+                letterSpacing: 1.0,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              "HEMA BHAGNANI",
+              style:TextStyle(
+                color: Colors.black,
+                letterSpacing: 1.0,
+                fontSize: 20.0,
+
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Row(
+              children: <Widget>[
+                Icon(Icons.mail),
+                SizedBox(width: 3.0),
+                Text(
+                  "hemabhagnani5@gmail.com",
+                  style:TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 1.0,
+                    fontSize: 15.0,
+                  ),
+                ),
+
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.account_box),
+                SizedBox(width: 3.0),
+                Text(
+                  "hemabhagnani.pythonanywhere.com",
+                  style:TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 1.0,
+                    fontSize: 15.0,
+                  ),
+                ),
+
+              ],
+            ),
+            Divider(
+              height: 45.0,
+              color:Colors.black,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  color:Colors.indigo,
+                  padding: EdgeInsets.fromLTRB(180.0, 10.0, 160.0, 10.0),
+                  child:Center(
+                    child: Text(
+                      "About",
+                      style: TextStyle(
+                        color:Colors.white,
+                      ),
+                    ),
+                  )
+                ),
+        ]
+      ),
+        SizedBox(height: 10.0,),
+
+                Row(
+                  children: <Widget>[
+                    Container(
+                      margin:EdgeInsets.all(10.0),
+                      child:Text("Hema Bhagnani is a 3rd-year undergraduate."),
+
+                    ),
+                  ],
+
+
+                ),
+              ],
+            )
+        ),
+      );
+
+  }
+}
